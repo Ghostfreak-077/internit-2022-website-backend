cde9eac6661ccc716bd245d8627e1b4f7f52eb99
Aditya Verma <123adiverma@gmail.com>
2022-10-30 16:51:45 +0530

    routes ready

---
+++ controllers/matchController.js
@@ -0,0 +1,119 @@
+const Match = require("../models/matchModel"); //schema
+const Team = require("../models/teamModel"); //schema
+
+//create match
+exports.createMatch = async (req, res, next) => {
+    try {
+        const match = await Match.create(req.body);
+        res.status(201).json({
+            success: true,
+            match
+        })
+    } catch (err) {
+        res.send(err.message);
+    }
+}
+
+//update match
+exports.updateMatch = async (req, res, next) => {
+    try {
+        let match = await Match.findById(req.params.id);
+        if (!match) {
+            return res.status(500).json({
+                success: false,
+                message: "match not found"
+            })
+        }
+        match = await Match.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+        });
+
+        if (req.body.team1Point + req.body.team2Point == 3) {
+            console.log(match);
+            let team1 = await Team.findOne({ teamName: match.team1 });
+            let team2 = await Team.findOne({ teamName: match.team2 });
+        
+            if (req.body.team1Point < req.body.team2Point) {
+                [team1,team2]=[team2,team1];
+            }
+            await Team.findOneAndUpdate({ teamName: team1.teamName }, { wins: team1.wins + 1, gamePlayed: team1.gamePlayed + 1 }, {
+                new: true,
+                runValidators: true,
+                useFindAndModify: false
+            });
+            await Team.findOneAndUpdate({ teamName: team2.teamName }, { gamePlayed: team2.gamePlayed + 1 }, {
+                new: true,
+                runValidators: true,
+                useFindAndModify: false
+            });
+
+        }
+        res.status(200).json({
+            success: true,
+            match
+        })
+    } catch (err) {
+        res.send(err.message);
+    }
+}
+
+//Delete match
+exports.deleteMatch = async (req, res, next) => {
+    try {
+        const match = await Match.findById(req.params.id);
+        if (!match) {
+            return res.status(500).json({
+                success: false,
+                message: "match not found"
+            })
+        }
+
+        await match.remove();
+
+        res.status(200).json({
+            success: true,
+            message: "match deleted"
+        })
+    } catch (err) {
+        res.send(err.message);
+    }
+}
+
+
+//Get one match Detail
+exports.getMatchDetails = async (req, res, next) => {
+    try {
+        const match = await Match.findById(req.params.id);
+        if (!match) {
+            return res.status(404).json({
+                success: false,
+                message: "match not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            match,
+        })
+    } catch (err) {
+        res.send(err.message);
+    }
+}
+
+
+//get all match
+exports.getAllMatchs = async (req, res) => {
+    try {
+        const matchsCount = await Match.countDocuments();
+        const matchs = await Match.find();
+        res.status(201).json({
+            success: true,
+            matchs,
+            matchsCount,
+        })
+    } catch (err) {
+        res.send(err.message);
+    }
+}
---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+
+const connectDataBase = require("./config/database");
+
+//connecting to database
+connectDataBase();
+
+//config
+if(process.env.NODE_ENV!=="PRODUCTION"){
+    require("dotenv").config({ path: "./config/.env" });
+}
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`server is working on port ${process.env.PORT}`)
+})
